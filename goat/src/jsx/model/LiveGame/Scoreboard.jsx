d375b447731b03efda4fc2ebef53d05ed18b78d5
matu_ <matu_diaz_95@hotmail.com>
2023-02-21 21:54:52 -0300

    LiveGame and Score

---
+++ goat/src/jsx/model/LiveGame/Scoreboard.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState, createRef, useRef, ErrorBoundary}  from 'react';
+import '../../../css/model/LiveGame.css'
+
+const Scoreboard = ({ home_score,
+    away_score,
+    home_label,
+    away_label, }) => {
+
+    return(
+        <>
+            <div className="scoreBoard">
+                <div className="HomeTeam">{home_label}</div>
+                <div className="ScoreHome">{home_score}</div>
+                <div className="BlankSpace"></div>
+                <div className="AwayTeam">{away_label}</div>
+                <div className="ScoreAway">{away_score}</div>
+            </div>
+        </>
+    )
+}
+export default Scoreboard
